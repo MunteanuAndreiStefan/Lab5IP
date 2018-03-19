4d73126fcd803cc8ae900c8a215b95ac0839d7a0
DoubleNy <1doubleny@gmail.com>
2018-03-19 07:27:19 +0300

    up2

--- src/Singleton1.java
+++ src/Singleton1.java
@@ -3,10 +3,16 @@ import java.io.Serializable;
 
 public final class Singleton1 implements SingletonMarker {
 
-public static final Singleton1 INSTANCE = new Singleton1();
+private static Singleton1 INSTANCE = null
 
 private Singleton1() {};
 
 public synchronized int mySyncMethod(){System.out.println("Sample text."); return 1;}
+
+public Singleton1 getInstance(){
+    if(INSTANCE == null)
+            INSTANCE = new Singleton1();
+            return INSTANCE;
+    }
 
 }
